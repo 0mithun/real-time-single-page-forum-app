da536202e16e64e141d2ec76f04f93e4d39c2f9d
Mithun Halder <mithunrptc@gmail.com>
2019-02-27 00:59:04 +0600

    JWT Package Added

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,5 @@
 <?php
-
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 
 Route::apiResource('/question','QuestionController');
 
@@ -26,4 +9,13 @@ Route::apiResource('/question/{question}/reply','ReplyController');
 
 Route::post('/like/{reply}','LikeController@likeIt');
 Route::delete('/like/{reply}','LikeController@unlikeIt');
+
+
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
 
