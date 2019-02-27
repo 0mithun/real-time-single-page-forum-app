b9f583ed8269f2898928a86736d90ed1234163d6
Mithun Halder <mithunrptc@gmail.com>
2019-02-28 00:26:22 +0600

    Login Component and Login added

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -2,11 +2,11 @@
     <v-toolbar>
         <v-toolbar-title>Mithu-It</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat>Forum</v-btn>
-            <v-btn flat>Ask Question</v-btn>
-            <v-btn flat>Category</v-btn>
-            <v-btn flat>Login</v-btn>
-        </v-toolbar-items>
+        <div class="hidden-sm-and-down">
+            <router-link to="#"><v-btn flat>Forum</v-btn></router-link>
+            <router-link to="#"><v-btn flat>Ask Question</v-btn></router-link>
+            <router-link to="#"><v-btn flat>Category</v-btn></router-link>
+            <router-link to="/login"><v-btn flat>Login</v-btn></router-link>
+        </div>
     </v-toolbar>
 </template>
